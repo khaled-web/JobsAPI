ab6d411ba28b810abe1ce93773755da7323748ac
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2022-03-10 16:00:39 +0200

    activate secure packages(helmet, cors, xss, rateLimit)

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 //dotenv...to secure you database
 require('dotenv').config();
 require('express-async-errors');
+
+//extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -19,8 +26,14 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 //middleware...to use req.body
 app.use(express.json());
-// extra packages
-
+// activate secure packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter({
+  windowMs: 60 * 1000,
+  max: 60
+}));
 // Activate authRouters
 app.use('/domain/api/v1/auth', authRouters);
 //Activate jobsRouters
